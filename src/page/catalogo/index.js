aa089539c8bc64fc550359558bea8f112c4d4c22
Maximiliano Gachon <gachonmaximiliano@outlook.es>
2023-09-19 15:24:17 -0300

    2 PreEntrega

--- src/page/catalogo/index.js
+++ src/page/catalogo/index.js
@@ -2,6 +2,7 @@ import "./style.css"
 import { useEffect, useState } from "react"
 import { getProducts } from "../../array"
 import { Link } from "react-router-dom"
+
 //import { getRate } from "../../utils/getRate"
 
 const Prendas = () => {
